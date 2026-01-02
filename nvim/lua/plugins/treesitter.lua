2f5c6729593fe694c30f95e24e84c5a61277f4b0
Wetar <azrilharis75@gmail.com>
2026-01-03 01:11:00 +0700

    Use yati.nvim to manage indentation for tsx files

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,35 +1,48 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	config = function()
-		local configs = require("nvim-treesitter.configs")
+	{
+		"yioneko/nvim-yati",
+		version = "*",
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter",
+		},
+	},
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			local configs = require("nvim-treesitter.configs")
 
-		configs.setup({
-			ensure_installed = {
-				"c",
-				"lua",
-				"vim",
-				"vimdoc",
-				"javascript",
-				"html",
-				"css",
-				"typescript",
-				"tsx",
-				"go",
-				"asm",
-				"json",
-				"jsonc",
-			},
-			sync_install = true,
-			auto_install = true,
-			highlight = {
-				enable = true,
-				additional_vim_regex_highlighting = true,
-			},
-			indent = {
-				enable = true,
-				disable = { "tsx" },
-			},
-		})
-	end,
+			configs.setup({
+				ensure_installed = {
+					"c",
+					"lua",
+					"vim",
+					"vimdoc",
+					"javascript",
+					"html",
+					"css",
+					"typescript",
+					"tsx",
+					"go",
+					"asm",
+					"json",
+					"jsonc",
+				},
+				sync_install = true,
+				auto_install = true,
+				yati = {
+					enable = true,
+					suppress_conflict_warning = true,
+				},
+				highlight = {
+					enable = true,
+					additional_vim_regex_highlighting = true,
+				},
+				indent = {
+					enable = true,
+					disable = { "tsx" },
+				},
+			})
+		end,
+	},
 }
